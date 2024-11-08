f118b860a5782c1a46d24fde1c1aeb7186cf1b8d
Padmaa14 <padma_k43@outlook.com>
2024-11-08 16:02:19 +0530

    Citizen Service POC

---
+++ demo.citizenService/src/test/java/com/CitizenService/demo/citizenService/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CitizenService.demo.citizenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
